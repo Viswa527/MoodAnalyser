f81e4998a75a28966bcd86374154f3fdb036787b
Viswa527 <Viswa527@github.com>
2020-04-18 21:27:56 +0530

    [Viswa]Modified.Message return Sad or Happy based on Mood

--- src/test/java/MoodAnalyserTesting.java
+++ src/test/java/MoodAnalyserTesting.java
@@ -4,11 +4,25 @@ import org.junit.Test;
 
 public class MoodAnalyserTesting {
     @Test
-    public void givenMessage_ContainsHappyMood_ReturnsTrue()
+    public void givenMessage_ContainsSadMood_ReturnsTrue()
     {
         MoodAnalyser moodAnalyser=new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("Sad",mood);
+    }
+    @Test
+    public void givenMessage_ContainsHappyMood_ReturnTrue()
+    {
+        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        Assert.assertEquals("Happy",mood);
+    }
+    @Test
+    public void givenMessage_ContainsAnyMood_ReturnsTrue()
+    {
+        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("Happy",mood);
     }
 
 }
